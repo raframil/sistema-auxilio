7b2e4bf6d501ff4123226a77273010de7e1b9b1e
Rafael Framil <raframil@gmail.com>
2018-10-27 15:46:48 -0300

    docker config...

---
+++ laravel/run.bat
@@ -0,0 +1,21 @@
+
+echo Uploading Application container 
+docker-compose up -d
+
+echo Copying the configuration example file
+docker exec -it sad-app cp .env.example .env
+
+echo Install dependencies
+docker exec -it sad-app composer install
+
+echo Generate key
+docker exec -it sad-app php artisan key:generate
+
+echo Make migrations
+docker exec -it sad-app php artisan migrate
+
+echo Make seeds
+docker exec -it sad-app php artisan db:seed
+
+echo Information of new containers
+docker ps -a 
