fa395dc1de2f386a2f2a8bab65e641b6eebb61b9
Rafael Framil <raframil@gmail.com>
2018-10-27 21:52:20 -0300

    Tipo de funcionario CRUD

--- laravel/routes/backpack/custom.php
+++ laravel/routes/backpack/custom.php
@@ -12,4 +12,5 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     CRUD::resource('doenca', 'DoencaCrudController');
+    CRUD::resource('tipo_funcionario', 'TipoFuncionarioCrudController');
 }); // this should be the absolute last line of this file
